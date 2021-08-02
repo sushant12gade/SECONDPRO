8b8005032ddd8ef84a115b0d2da843f4473c1473
sushant12gade <87536506+sushant12gade@users.noreply.github.com>
2021-08-02 16:31:00 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'second.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'D:\secondpro\second\templates'],
+        'DIRS': [******],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
